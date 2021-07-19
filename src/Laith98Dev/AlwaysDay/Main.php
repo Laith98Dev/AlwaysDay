4de6e4c36d5db7b89d81e6efbacb74791a5182b2
Laith Youtuber <49840784+Laith98Dev@users.noreply.github.com>
2021-07-19 04:48:10 -0700

    Update Main.php

--- src/Laith98Dev/AlwaysDay/Main.php
+++ src/Laith98Dev/AlwaysDay/Main.php
@@ -2,6 +2,38 @@
 
 namespace Laith98Dev\AlwaysDay;
 
+/*  
+ *  A plugin for PocketMine-MP.
+ *  
+ *	 _           _ _   _    ___   ___  _____             
+ *	| |         (_) | | |  / _ \ / _ \|  __ \            
+ *	| |     __ _ _| |_| |_| (_) | (_) | |  | | _____   __
+ *	| |    / _` | | __| '_ \__, |> _ <| |  | |/ _ \ \ / /
+ *	| |___| (_| | | |_| | | |/ /| (_) | |__| |  __/\ V / 
+ *	|______\__,_|_|\__|_| |_/_/  \___/|_____/ \___| \_/  
+ *	
+ *	Copyright (C) 2021 Laith98Dev
+ *  
+ *	Youtube: Laith Youtuber
+ *	Discord: Laith98Dev#0695
+ *	Gihhub: Laith98Dev
+ *	Email: help@laithdev.tk
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ */
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\level\LevelLoadEvent;
@@ -19,6 +51,8 @@ class Main extends PluginBase implements Listener
 			if(in_array($name, [".", ".."]))
 				continue;
 			$this->getServer()->loadLevel($name);
+			$this->getServer()->getLevelByName($name)->setTime(Level::TIME_DAY);
+			$this->getServer()->getLevelByName($name)->stopTime();
 		}
 	}
 	
